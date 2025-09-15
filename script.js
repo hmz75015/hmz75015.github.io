feae779020a08063a13a06df73f907a1ddded9d3
hmz75015 <hmz75xv@gmail.com>
2025-09-16 01:33:28 +0200

    Premier commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Animation des barres de compétences
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            }
+        });
+    });
+
+    document.querySelectorAll('.progress').forEach((progress) => {
+        observer.observe(progress);
+    });
+});
+
+// Animation douce du défilement
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
